cd4e689712e1456dabdd3f3ba07f06b319c9563a
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-03-15 00:25:59 +0500

    main

--- src/app/component/CheckedAnswer.tsx
+++ src/app/component/CheckedAnswer.tsx
@@ -26,7 +26,7 @@ export default function CheckedAnswer({res} : IProps) {
         )
       }else{
         return (
-           <div  className='bg-gray-100 border-2 p-5 font-extrabold' key={i}>
+           <div  className='bg-gray-100 border-2 p-5 font-bold' key={i}>
             Answer : {val.answer}
            </div> 
         )
